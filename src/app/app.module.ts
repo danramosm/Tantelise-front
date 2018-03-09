7dba558ed014e5fadb72a0132d342eac6b64727a
Daniel Ramos Montes <danramosm@gmail.com>
2018-03-09 12:34:53 -0300

    estructura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PagesComponent } from './publico/pages/pages.component';
+import { SharedComponent } from './publico/shared/shared.component';
+import { HeaderComponent } from './publico/shared/header/header.component';
+import { FooterComponent } from './publico/shared/footer/footer.component';
+import { InicioComponent } from './publico/pages/inicio/inicio.component';
+import { ProductosComponent } from './publico/pages/productos/productos.component';
+import { ContactoComponent } from './publico/pages/contacto/contacto.component';
+import { ServiciosComponent } from './publico/pages/servicios/servicios.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    ProductosComponent,
+    ContactoComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule
